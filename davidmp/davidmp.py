8b7df0ea8bd054dbef4da63e6089c5020fadfe16
javiteri <javiteri80@gmail.com>
2015-01-25 09:55:35 -0500

    Cambio de Proyecto del Profe

--- davidmp/davidmp.py
+++ davidmp/davidmp.py
@@ -1,6 +1,6 @@
 '''
 Created on 24/2/2015
-
+Segundo Cambio en proyecto Mamani
 @author: PC06
 '''
 
---
+++ davidmp/milton.py
@@ -0,0 +1,8 @@
+'''
+Created on 25/1/2015
+
+@author: USUARIO
+'''
+
+if __name__ == '__main__':
+    pass
